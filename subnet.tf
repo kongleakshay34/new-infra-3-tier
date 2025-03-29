831f87213cee4f73fec12c217f9d2b0e2bf29b1f
kongleakshay34 <kongleakshay34@gmail.com>
2025-03-29 11:11:10 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,4 +1,4 @@
-# Public Subnets 
+# WEB-SERVER PUBLIC-SUBNET-1
 resource "aws_subnet" "swiggy-pub-sub-1" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.0/28"
@@ -10,6 +10,7 @@ resource "aws_subnet" "swiggy-pub-sub-1" {
   }
 }
 
+# WEB-SERVER PUBLIC-SUBNET-2
 resource "aws_subnet" "swiggy-pub-sub-2" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.16/28"
@@ -20,43 +21,47 @@ resource "aws_subnet" "swiggy-pub-sub-2" {
   }
 }
 
-
-# Private Subnets
+# APP-SERVER PRIVATE-SUBNET-1
 resource "aws_subnet" "swiggy-pvt-sub-1" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.32/28"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = false
   tags = {
-    Name = "swiggy-pvt-sub-1"
+    Name = "swiggy-app-sub-1"
   }
 }
+
+# APP-SERVER PRIVATE-SUBNET-2
 resource "aws_subnet" "swiggy-pvt-sub-2" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.48/28"
   availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = false
   tags = {
-    Name = "swiggy-pvt-sub-2"
+    Name = "swiggy-app-sub-2"
   }
 }
 
+# DB-SERVER PRIVATE-SUBNET-1
 resource "aws_subnet" "swiggy-pvt-sub-3" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.64/28"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = false
   tags = {
-    Name = "swiggy-pvt-sub-3"
+    Name = "swiggy-db-sub-1"
   }
 }
+
+# DB-SERVER PRIVATE-SUBNET-2
 resource "aws_subnet" "swiggy-pvt-sub-4" {
   vpc_id                  = aws_vpc.swiggy-vpc.id
   cidr_block              = "10.0.0.80/28"
   availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = false
   tags = {
-    Name = "swiggy-pvt-sub-4"
+    Name = "swiggy-db-sub-2"
   }
 }
 
